672ea50e5df40852453de5bc63728182ae041888
mineer22 <mineer22@users.noreply.github.com>
2020-09-29 16:03:00 +0200

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,15 @@
+from __future__ import print_function
+
+import json
+
+print('Loading function')
+
+
+def lambda_handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+    #print("value1 = " + event['key1'])
+    #print("value2 = " + event['key2'])
+    #print("value3 = " + event['-'])
+    #return event['key1']  # Echo back the first key value
+    return ("hello from ",  json.dumps(event, indent=2))
+    #raise Exception('Something went wrong')
